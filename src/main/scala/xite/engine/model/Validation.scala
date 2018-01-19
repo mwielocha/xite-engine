45c2ccfd16e31faddbdcca8de6c1ce263bfb8049
mwielocha <mwielocha@icloud.com>
2018-01-19 22:11:29 +0100

    cleanups part 3

--- src/main/scala/xite/engine/model/Validation.scala
+++ src/main/scala/xite/engine/model/Validation.scala
@@ -1,12 +1,12 @@
 package xite.engine.model
 
-import cats.data.{NonEmptyList, Validated}
+import cats.data.Validated
 import cats.data.Validated.{Invalid, Valid}
 
 trait Validation {
 
   def check[T](in: T)(condition: T => Boolean, error: String): Validated[Errors, T] = {
-    if(condition(in)) Valid(in) else Invalid(NonEmptyList.of(error))
+    if(condition(in)) Valid(in) else Invalid(Errors(error))
   }
 }
 
